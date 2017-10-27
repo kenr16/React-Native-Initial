edd9c617cc1a1d835d973fb0dceae98e9f1bde01
Ken Rutan <kenr16@gmail.com>
2017-10-26 22:18:29 -0700

    modified hello world app

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Image, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>"Open up App.js to start working on your app!"</Text>
-        <Text>"Changes you make will automatically reload."</Text>
-        <Text>"Shake your phone to open the developer menu."</Text>
-      </View>
+      <Text>Hello {this.props.name}!</Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class LotsOfGreetings extends Component {
+  render() {
+    let styles = {alignItems: 'center', marginTop: 100}
+    return (
+      <View style={styles}>
+        <Greeting name='Rexxar'/>
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera'/>
+      </View>
+    );
+  }
+}
+// export default class HelloWorldApp extends Component {
+//   render() {
+//     let pic = {
+//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     };
+//     return (
+//       <Image source={pic} style={{width: 193, height: 110}}/>
+//     );
+//   }
+// }
