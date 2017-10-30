bf15699be0c12db2121f6cf206d611480578aec6
Ken Rutan <kenr16@gmail.com>
2017-10-29 20:36:37 -0700

    passing image links via props

--- App.js
+++ App.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
 class Greeting extends Component {
+
   render() {
+
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <View>
+        <Text>{this.props.name}!</Text>
+        <Image source={{ uri: this.props.pic }} style={{width: 200, height: 150, boxShadow: "10px 10px 5px #888888"}}/>
+      </View>
     );
   }
 }
 
 export default class LotsOfGreetings extends Component {
+
   render() {
     let styles = {alignItems: 'center', marginTop: 100}
     return (
       <View style={styles}>
-        <Greeting name='Rexxar'/>
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera'/>
+        <Greeting name='1986 Raiders Jacket' pic='http://s7d1.scene7.com/is/image/ShiekhShoes/19-0676.1A?$shiekh_large$'/>
+        <Greeting name='Pink Sweatervest' pic='https://cdnc.lystit.com/photos/2013/02/20/brooks-brothers-pink-intarsia-sweater-vest-product-1-6567030-719884497.jpeg' />
+        <Greeting name='Rainbow Leggings' pic='https://rlv.zcache.com/rainbow_flag_leggings-r55446fff4e70446fb3bca6fbcf838ebb_68vic_324.jpg?rlvnet=1' />
+        <Greeting name='Plaid Skirt' pic='https://cdn.shopify.com/s/files/1/1117/2438/products/dotcomme41_copy_grande.jpg?v=1465780640' />
+        <Greeting name='80s Parachute Pants' pic='https://cdn-img-2.wanelo.com/p/e3c/da4/c19/1575006c506844fe68e80b6/x354-q80.jpg' />
       </View>
     );
   }
