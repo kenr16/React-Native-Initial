0e7a71c9903630f290ab5cb6af4a646ac1f79126
Ken Rutan <kenr16@gmail.com>
2017-11-01 12:58:52 -0700

    further working with the flexbox in React-native

--- App.js
+++ App.js
@@ -1,40 +1,30 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, Text, View } from 'react-native';
+import { AppRegistry, Image, Text, View, StyleSheet } from 'react-native';
+// ****************************
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {showText: true};
 
-    //Toggle the state every second
-    setInterval( () => {
-      this.setState(previousState => {
-        return { showText: !previousState.showText };
-      });
-    }, 1000);
-  }
 
-  render() {
-    let display = this.state.showText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    )
-  }
-}
 
-export defaul class BlinkApp extends Component {
+// ****************************
+
+export default class FlexDirectionsBasics extends Component {
   render() {
     return (
-      <View>
-        <Blink text='I love to blink!' />
-        <Blink text='Yes blinking is so great!' />
-        <Blink text='Why did they ever take this out of HTML?' />
-        <Blink text='Look at me look at me look at me' />
+      // justifyContent 'flex-start', 'center', 'flex-end', 'space-around', 'space-between'
+      // alignItems 'flex-start', 'center', 'flex-end', 'stretch'
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
-}
-
+};
 
 // ****************************
 // class Greeting extends Component {
@@ -65,16 +55,4 @@ export defaul class BlinkApp extends Component {
 //     );
 //   }
 // }
-// *****************************
-
-
-// export default class HelloWorldApp extends Component {
-//   render() {
-//     let pic = {
-//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-//     };
-//     return (
-//       <Image source={pic} style={{width: 193, height: 110}}/>
-//     );
-//   }
-// }
+// ****************************
